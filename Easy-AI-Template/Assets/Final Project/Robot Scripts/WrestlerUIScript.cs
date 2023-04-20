6dc305c4f07fd76c57c72894b36f72cf9b5e5091
r-bjorn <ravidtrivedi111@gmail.com>
2023-04-20 17:18:55 -0400

    Add User Input Behaviour UI

--- Easy-AI-Template/Assets/Final Project/Robot Scripts/WrestlerUIScript.cs
+++ Easy-AI-Template/Assets/Final Project/Robot Scripts/WrestlerUIScript.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using Final_Project.Robot_Types;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,44 +7,60 @@ namespace Final_Project.Robot_Scripts
 {
     public class WrestlerUIScript : MonoBehaviour
     {
-        // public GameObject player;
-        // public RandomAIScript randomAI;
-        // public MovingToHealthLocationScript movingToHealthLocation;
-        // public AttackingAIScript attackingAI;
-        //
-        // public void OnRadioButtonSelected(int index)
-        // {
-        //     switch (index)
-        //     {
-        //         case 0:
-        //             randomAI.enabled = true;
-        //             movingToHealthLocation.enabled = false;
-        //             attackingAI.enabled = false;
-        //             break;
-        //         case 1:
-        //             randomAI.enabled = false;
-        //             movingToHealthLocation.enabled = true;
-        //             attackingAI.enabled = false;
-        //             break;
-        //         case 2:
-        //             randomAI.enabled = false;
-        //             movingToHealthLocation.enabled = false;
-        //             attackingAI.enabled = true;
-        //             break;
-        //     }
-        // }
-    
-        private ToggleGroup _toggleGroup;
+        public GameObject player;
 
+        private RobotRoamingAI _roamingAI;
+        private MedicRobotAI _medicAI;
+        private AttackingRobotAI _attackAI;
+        
+        private ToggleGroup _toggleGroup;
+        
+        private void OnRadioButtonSelected(string index)
+        {
+            switch (index)
+            {
+                case "     Random AI Behaviour":
+                    _medicAI.enabled = false;
+                    _attackAI.enabled = false;
+                    _roamingAI.enabled = true;
+                    break;
+                case "     Medic AI Behaviour":
+                    _attackAI.enabled = false;
+                    _roamingAI.enabled = false;
+                    _medicAI.enabled = true;
+                    break;
+                case "     Attacking AI Behaviour":
+                    _medicAI.enabled = false;
+                    _roamingAI.enabled = false;
+                    _attackAI.enabled = true;
+                    break;
+                default:
+                    _medicAI.enabled = false;
+                    _roamingAI.enabled = false;
+                    _attackAI.enabled = false;
+                    break;
+            }
+        }
+    
         private void Start()
         {
             _toggleGroup = GetComponent<ToggleGroup>();
+            
+            _roamingAI = player.GetComponent<RobotRoamingAI>();
+            _medicAI = player.GetComponent<MedicRobotAI>();
+            _attackAI = player.GetComponent<AttackingRobotAI>();
+            
         }
 
         public void Submit()
         {
             Toggle toggle = _toggleGroup.ActiveToggles().FirstOrDefault();
-            if (toggle is not null) Debug.Log(toggle.name + " _ " + toggle.GetComponentInChildren<Text>().text);
+
+            if (toggle is null) return; 
+            
+            Debug.Log(toggle.name + " _ " + toggle.GetComponentInChildren<Text>().text);
+            
+            OnRadioButtonSelected(toggle.GetComponentInChildren<Text>().text);
         }
     }
 }
---
+++ Easy-AI-Template/Assets/Final Project/Robot Types/MedicRobotAI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedicRobotAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
